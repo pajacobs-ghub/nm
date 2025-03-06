a18ac222c7b926e1289d0c7882bc655cae388f28
Peter Jacobs <peterj@mech.uq.edu.au>
2025-03-06 21:17:18 +1000

    Introduce some vector arithmetic for vectors of arbitrary length.

---
+++ array/vector.go
@@ -0,0 +1,148 @@
+// vector.go
+// A package for working with vectors of numbers of arbitrary length.
+// The intention is to make the arithmetic code in nelmin a bit cleaner.
+//
+// PJ 2025-03-06
+
+package array
+
+import (
+	"fmt"
+	"math"
+	"bytes"
+)
+
+type Vector struct {
+	Data []float64
+}
+
+func (v Vector) IsEmpty() bool {
+	return len(v.Data) == 0
+}
+
+func VectorZeros(n int) Vector {
+	return Vector{Data: make([]float64, n)}
+}
+
+func VectorOnes(n int) Vector {
+	z := Vector{Data: make([]float64, n)}
+	for i := 0; i < n; i++ {
+		z.Data[i] = 1.0
+	}
+	return z
+}
+
+func VectorCopyArray(data []float64) Vector {
+	n := len(data)
+	z := Vector{Data: make([]float64, n)}
+	for i := 0; i < n; i++ {
+		z.Data[i] = data[i]
+	}
+	return z
+}
+
+func (a Vector) Clone() Vector {
+	n := len(a.Data)
+	z := Vector{Data: make([]float64, n)}
+	for i := 0; i < n; i++ {
+		z.Data[i] = a.Data[i]
+	}
+	return z
+}
+
+func (a Vector) String() string {
+	var b bytes.Buffer
+	n := len(a.Data)
+	b.WriteString("(")
+	for i, d := range a.Data {
+		b.WriteString(fmt.Sprintf("%0.6f", d))
+		if i+1 < n {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteString(")")
+	return b.String()
+}
+
+func (a Vector) Sum() float64 {
+	s := 0.0
+	for _, d := range a.Data {
+		s += d
+	}
+	return s
+}
+
+func (a Vector) Mean() float64 {
+	n := len(a.Data)
+	if n == 0 {
+		return 0.0
+	}
+	s := 0.0
+	for _, d := range a.Data {
+		s += d
+	}
+	return s/float64(n)
+}
+
+func (a Vector) ApproxEquals(other Vector, tol float64) bool {
+	n := len(a.Data)
+	if n != len(other.Data) {
+		return false
+	}
+	for i := 0; i < n; i++ {
+		if math.Abs(a.Data[i]-other.Data[i]) > tol {
+			return false
+		}
+	}
+	return true
+}
+
+func (z *Vector) Scale(a float64) *Vector {
+	for i := 0; i < len(z.Data); i++ {
+		z.Data[i] *= a
+	}
+	return z
+}
+
+// For the arithmetic function signatures, use the math/big package as a model.
+// If results are always pre-allocated, we should have better control
+// of the memory required for our expressions.
+// Also, we allow aliasing of the arguments so that we can achieve certain
+// effects, e.g. z = z + a can be obtained as z = z.Add(z,a)
+
+func (z *Vector) Add(a, b *Vector) *Vector {
+	n := len(z.Data)
+	if n != len(a.Data) || n != len(b.Data) {
+		panic(fmt.Sprintf("Inconsistent array lengths z:%v a:%v b:%v",
+			len(z.Data), len(a.Data), len(b.Data)))
+	}
+	for i := 0; i < n; i++ {
+		z.Data[i] = a.Data[i] + b.Data[i]
+	}
+	return z
+}
+
+func (z *Vector) Sub(a, b *Vector) *Vector {
+	n := len(z.Data)
+	if n != len(a.Data) || n != len(b.Data) {
+		panic(fmt.Sprintf("Inconsistent array lengths z:%v a:%v b:%v",
+			len(z.Data), len(a.Data), len(b.Data)))
+	}
+	for i := 0; i < n; i++ {
+		z.Data[i] = a.Data[i] - b.Data[i]
+	}
+	return z
+}
+
+func VectorDot(a, b *Vector) float64 {
+	n := len(a.Data)
+	if n != len(b.Data) {
+		panic(fmt.Sprintf("Inconsistent array lengths a:%v b:%v",
+			len(a.Data), len(b.Data)))
+	}
+	s := 0.0
+	for i := 0; i < n; i++ {
+		s += a.Data[i] * b.Data[i]
+	}
+	return s
+}
---
+++ array/vector_test.go
@@ -0,0 +1,55 @@
+/** vector_test.go
+ * Try out some of the Vector functions.
+ * PJ 2025-03-06
+ */
+
+package array
+
+import (
+	"testing"
+	_ "fmt"
+	"math"
+)
+
+func TestVector(t *testing.T) {
+	v0 := Vector{}                         // Empty Vector.
+	// fmt.Println("v0=", v0.String())
+	if !v0.IsEmpty() {
+		t.Errorf("Vector v0 appears to be not empty.")
+	}
+	v1 := Vector{[]float64{1.0, 2.0, 3.0}} // One way to initialize.
+	// fmt.Println("v1=", v1.String())
+	if len(v1.Data) != 3 || v1.Data[0] != 1.0 {
+		t.Errorf("Vector initialize error v1= %v want= (1.0, 2.0, 3.0)", v1.String())
+	}
+	v2 := VectorZeros(4)                         // Another way.
+	// fmt.Println("v2=", v2.String())
+	if len(v2.Data) != 4 || v2.Data[0] != 0.0 {
+		t.Errorf("Vector initialize error v2= %v want= (0.0, 0.0, 0.0, 0.0)", v2.String())
+	}
+	v3 := VectorOnes(4)                          // And another way.
+	// fmt.Println("v3=", v3.String())
+	if len(v3.Data) != 4 || v3.Sum() != 4.0 { // Exactly-representable numbers used.
+		t.Errorf("Vector initialize error v3= %v want= (1.0, 1.0, 1.0, 1.0)", v3.String())
+	}
+	m := v3.Mean()
+	if m != 1.0 { // Exactly-representable numbers used.
+		t.Errorf("Vector Mean error m= %v want= 1.0", m)
+	}
+	v4 := VectorCopyArray([]float64{1.1, 2.2, 3.3, 4.4})
+	v5 := v4.Clone()
+	// fmt.Println("v4=", v4.String())
+	if len(v4.Data) != 4 || !v4.ApproxEquals(v5, 1.0e-9) {
+		t.Errorf("Vector clone error v5= %v want= %v", v5.String(), v4.String())
+	}
+	v6 := VectorZeros(4)
+	v6.Add(&v4, &v5)
+	v5.Scale(2.0)
+	if len(v6.Data) != 4 || !v6.ApproxEquals(v5, 1.0e-9) {
+		t.Errorf("Vector clone error v6= %v want= %v", v6.String(), v5.String())
+	}
+	s := VectorDot(&v3, &v3)
+	if math.Abs(s - 4.0) > 1.0e-9 {
+		t.Errorf("Vector dot product error s= %v want= %v", s, 4.0)
+	}
+}
